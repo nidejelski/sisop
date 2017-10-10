01063af1373c1f7779ff713db32049a94fe03e70
nidejelski <nidejelski@gmail.com>
2017-10-10 19:44:57 -0300

    Update main.c

--- cthread/src/main.c
+++ cthread/src/main.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "cthread.h"
+#include "cthread.c"
 #include "support.h"
 #include "escalonador.h"
 #include "escalonador.c"
+
+//testes
+void f1(){
+    printf ("\nFUNCAO 1 \n");
+}
+
+void f2(){
+    printf ("\nFUNCAO 2 \n");
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -33,16 +44,29 @@ int main(int argc, char *argv[])
 
     escalona = escalonadorInit();
 
-    if (escalona->filaAptos == NULL) {
-        printf("LISTA DE APTOS ESTA VAZIA\n");
-    }
-    if (escalona->filaBloqs == NULL) {
-        printf("LISTA DE BLOQS ESTA VAZIA\n");
-    }
-
-
+    if (NextFila2(escalona->filaAptos) == -1) printf ("\n Lista de Aptos Vazia! \n");
+    //if (escalona->filaAptos == NULL) {
+    //    printf("LISTA DE APTOS ESTA VAZIA\n");
+   // }
+   // if (escalona->filaBloqs == NULL) {
+   //     printf("LISTA DE BLOQS ESTA VAZIA\n");
+    //}
 
     //testes threads
+    //cria Thread 1
+    int t1 = ccreate((void*)f1,escalona,0);
+
+    if (t1 == 0) printf ("\n Nao conseguiu criar thread 1");
+    else printf ("\n Thread 1 criada!");
+
+    //cria Thread2
+    int t2 = ccreate((void*)f2,escalona,0);
+
+    if (t2 == 0) printf ("\n Nao conseguiu criar thread 1");
+    else printf ("\n Thread 2 criada!");
+
+    //FirstFila2(escalona->filaAptos);
+    //if (NextFila2(escalona->filaAptos) == -3 ) printf ("\n FIM DA FILA! \n");
 
     return 0;
 }
