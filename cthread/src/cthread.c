95eb92cd0ad7ad313a07992ceecdf31828dfcfff
nidejelski <nidejelski@gmail.com>
2017-10-10 19:46:23 -0300

    Update cthread.c

--- cthread/src/cthread.c
+++ cthread/src/cthread.c
@@ -8,6 +8,8 @@
 #define TAMANHO_PILHA 16384
 #define MAXIMO_THREAD 100
 
+int threadID = 0;
+
 /*--------------------------------------------------------------------
 Função: CCREATE
 Parâmetros:
@@ -20,36 +22,60 @@ Caso contrário, retorna um valor negativo.
 --------------------------------------------------------------------*/
 
 int ccreate(void* (*start)(void*), void *arg, int prio){
-	
-	ucontext_t* contextoNovo = (ucontext_t*) malloc(sizeof(ucontext_t));
 
-	s_TCB* t = (s_TCB*) malloc(sizeof(s_TCB));
+    ucontext_t G, *cp = &G;
+
+	ucontext_t *contextoNovo = malloc(sizeof(ucontext_t));
+
+	TCB_t* t = (TCB_t*) malloc(sizeof(TCB_t));
 	contextoNovo->uc_stack.ss_sp = (char*)malloc(TAMANHO_PILHA);
 	contextoNovo->uc_stack.ss_size = TAMANHO_PILHA;
-		
-	//No fim da função tem que retornar pro escalonador	
-	contextoNovo->uc_link = [CONTEXTO ESCALONADOR]; //algo do tipo: escalonador->contexto_escalonador 
+
+	//No fim da função tem que retornar pro escalonador
+	contextoNovo->uc_link = NULL;//[CONTEXTO ESCALONADOR]; //algo do tipo: escalonador->contexto_escalonador
 	getcontext(contextoNovo);
-	makecontext(contextoNovo,(void(*)(void))start,1,arg); 
-	
+	makecontext(contextoNovo,(void(*)(void))start,1,arg);
+
 	//Seta parametros da TCB
-	t->tid = id++;
+	threadID++;
+	t->tid = threadID;
 	t->state = PROCST_CRIACAO;
 	t->prio = prio;
-	t->context = contextoNovo;
-  
+
+    cp = contextoNovo;
+	//G = contextoNovo;
+	t->context = G;
+
+    printf ("\nTID: %d \n", t->tid);
+
+
+    //teste Insere APTOS
+    if (threadID == 1){
+        if (insereAptos(arg, t) == 0) printf ("\nSUCESSO APTOS\n");
+        else printf("\nFALHA APTOS\n");
+    }
+
+    //teste Insere BLOQS
+    else if (threadID == 2){
+        if (insereBloqs(arg, t) == 0) printf ("\nSUCESSO BLOQS\n");
+        else printf("\nFALHA BLOQS\n");
+    }
+
+
+	//return t->tid;
+
         //Conferência de maximo de threads (100)
 	//Algo do tipo: ( (escalonador->fila_aptos->size + escalonador->fila_bloqueados->size ) < MAXIMO_THREAD ))
-        if( ([NR. THREADS NA FILA DE APTOS] + [NR. THREADS NA FILA DE BLOQUEADOS]) < MAXIMO_THREAD){
+   //     if( ([NR. THREADS NA FILA DE APTOS] + [NR. THREADS NA FILA DE BLOQUEADOS]) < MAXIMO_THREAD){
                   //Insere na fila de APTOS
                   //insereAptos(escalonador, t);
-                  return t->tid;
-	}
-	else{
+    //              return t->tid;
+	//}
+	//else{
 		  //Erro
-		  printf("Excedido o numero maximo de threads - THREAD: %d não foi criada\n",t->tid); 	
-		  free(contextoNovo);
-		  free(t);	
-		  return -1;						 
-	}
+	//	  printf("Excedido o numero maximo de threads - THREAD: %d não foi criada\n",t->tid);
+//	  free(contextoNovo);
+//		  free(t);
+//		  return -1;
+//	}
 }
