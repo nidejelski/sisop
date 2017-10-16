45a3bfc2d42e6d09354a03f0683e9c9ceab632db
nidejelski <nidejelski@gmail.com>
2017-10-16 18:32:08 -0200

    Update main.c

--- cthread/src/main.c
+++ cthread/src/main.c
@@ -18,7 +18,7 @@ void f2(){
 
 int main(int argc, char *argv[])
 {
-    printf("Hello world!\n");
+    printf("INICIO DA MAIN!\n");
 
     //testes filas
     /*
@@ -44,7 +44,13 @@ int main(int argc, char *argv[])
 
     escalona = escalonadorInit();
 
-    if (NextFila2(escalona->filaAptos) == -1) printf ("\n Lista de Aptos Vazia! \n");
+    if (NextFila2(escalona->filaAptos) == -1) printf ("Lista de Aptos Vazia! \n");
+
+    //Testa qual thread está em execução
+    if (escalona->threadEmExec == NULL) printf ("\n THREAD EM EXECUCAO: NULL \n", escalona->threadEmExec);
+    else printf ("\n THREAD EM EXECUCAO: %d ", escalona->threadEmExec->tid);
+
+
     //if (escalona->filaAptos == NULL) {
     //    printf("LISTA DE APTOS ESTA VAZIA\n");
    // }
@@ -60,16 +66,46 @@ int main(int argc, char *argv[])
     else printf ("\n Thread %d criada!", t1);
 
     //cria Thread2
-    int t2 = ccreate((void*)f2,escalona,0);
+    int t2 = ccreate((void*)f2,escalona,1);
 
-    if (t2 == 0) printf ("\n Nao conseguiu criar thread 1");
+    if (t2 == 0) printf ("\n Nao conseguiu criar thread 2l");
     else printf ("\n Thread %d criada!", t2);
 
+    //testa se fila de APTOS está vazia após inserção
+    printf ("\n FILA APTOS ESTA VAZIA? \n");
+    if (FirstFila2(escalona->filaAptos) == 0) printf ("\n SELECIONADO PRIMEIRO ELEMENTO EM APTOS! \n");
+    TCB_t* thread_TESTE = GetAtIteratorFila2(escalona->filaAptos);
+
+    printf ("\nAtributos da Thread %d: \n", t1);
+    printf ("TID: %d \n", thread_TESTE->tid);
+    printf ("ESTADO: %d \n", thread_TESTE->state);
+    printf ("PRIORIDADE: %d \n", thread_TESTE->prio);
+
+
+    //testa se fila de BLOQS está vazia após inserção
+    printf ("\n FILA BLOQS ESTA VAZIA? \n");
+    if (FirstFila2(escalona->filaBloqs) == 0) printf ("\n SELECIONADO PRIMEIRO ELEMENTO EM BLOQS! \n");
+    TCB_t* thread_TESTE2 = GetAtIteratorFila2(escalona->filaBloqs);
+
+    printf ("\nAtributos da Thread %d: \n", t2);
+    printf ("TID: %d \n", thread_TESTE2->tid);
+    printf ("ESTADO: %d \n", thread_TESTE2->state);
+    printf ("PRIORIDADE: %d \n", thread_TESTE2->prio);
+
+
+    //teste de EXECUCAO DA THREAD
+    escalonadorExec();
+
+    //testa qual Thread esta em execucao no momento
+    if (escalona->threadEmExec == NULL) printf ("\n THREAD EM EXECUCAO: NULL \n", escalona->threadEmExec);
+    else printf ("\n THREAD EM EXECUCAO: %d ", escalona->threadEmExec->tid);
+
+
     //teste Remove APTOS
-    if (removeAptos(escalona) == 0) printf ("\n SUCESSO REMOVE APTOS \n");
+    //if (removeAptos(escalona) == 0) printf ("   SUCESSO REMOVE APTOS \n");
 
     //teste Remove BLOQS
-    if (removeBloqs(escalona) == 0) printf ("\n SUCESSO REMOVE BLOQS \n");
+    if (removeBloqs(escalona) == 0) printf ("   SUCESSO REMOVE BLOQS \n");
 
 
     if (NextFila2(escalona->filaAptos) == -1) printf ("\n Lista de Aptos Vazia! \n");
